67e1017272e45733d92458305165143d93ed72ea
ays19 <sahsanyasir@gmail.com>
2026-06-27 07:09:29 +0600

    chore: add gitignore, update tsconfig, and generate package-lock

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Sharar-s-portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
